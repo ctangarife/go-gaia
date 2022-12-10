22de73d4b20be419dd80c57211730f7144ef275a
ctangarife <cgt9145@gmail.com>
2022-12-10 15:55:08 -0500

    Commit from base structure fro gaia project

---
+++ gpt3/gtp.go
@@ -0,0 +1,34 @@
+package gtp
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/joho/godotenv"
+)
+
+func Gtp(text string) {
+	godotenv.Load()
+
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatalln("Missing API KEY")
+	}
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+
+	resp, err := client.Completion(ctx, gpt3.CompletionRequest{
+		Prompt:    []string{text},
+		MaxTokens: gpt3.IntPtr(30),
+		Stop:      []string{"."},
+		Echo:      true,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(resp.Choices[0].Text)
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	gtp "github.com/ctangarife/gaia/gtp3"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	resp := gtp.Gtp("The first thing you should know about javascript is")
+}
